4f8deea6ceb506de140a1cb8dca7eaee8960ae33
Lorenzo Faccioli <lorenzo.faccioli@octopeek.com>
2023-09-14 16:51:04 +0200

    Implemented Kp scraping

---
+++ utils/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='kp_utils_raw',
+      version='1.0',
+      py_modules=['kp_utils_raw'],
+)
+
+# setup(name='ace_utils_silver',
+#       version='1.0',
+#      py_modules=['ace_utils_silver'],
+#)
